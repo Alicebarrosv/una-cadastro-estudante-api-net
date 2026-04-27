5290201224e23f10d10cf7344974289269f9be70
Alicebarrosv <alicebarros152152@gmail.com>
2026-04-27 20:18:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using CadastroEstudanteApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source = estudantes.db"));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwagger();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
